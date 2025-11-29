5c2fe34fd3deeb244e965c369448dbc3c6790b10
mortezamsp <mortezamsp.2014@gmail.com>
2025-11-29 16:42:10 +0330

    bug fix 2

--- src/fmm/interaction.jl
+++ src/fmm/interaction.jl
@@ -1,5 +1,11 @@
 export M2L!, P2P!, interact!
 
+	
+struct PartialTimingResults
+	M2L_time::Float64
+	P2P_time::Float64
+end
+
 function M2L!(mp::MacroParticles{I,T}, ct::ClusterTree{I,T}, m2l_lists::Vector{Tuple{I,I}}, nm2l::I; p_avg::SVector{3,T}) where {I,T}
     n = mp.n
     bboxes = ct.clusters.bboxes
@@ -85,11 +91,6 @@ function interact!(mp::MacroParticles{I,T}, ct::ClusterTree{I,T}, itlists::Inter
 	P2P!(ct.particles, ct.parindices, ct.clusters, itlists.p2p_lists, itlists.np2p)
 	end_time = Dates.now()
     P2P_time = Float64(Dates.value(end_time - start_time)) / 1000.0  # Convert to seconds
-	
-	struct PartialTimingResults
-		M2L_time::Float64
-		P2P_time::Float64
-	end
-	
+
 	return PartialTimingResults(M2L_time, P2P_time)
 end
