232a494729dfdf024cd38d86d74d306bd962a7bc
mortezamsp <mortezamsp.2014@gmail.com>
2025-11-29 19:04:04 +0330

    bug fix 2

--- src/update_particles_field.jl
+++ src/update_particles_field.jl
@@ -27,9 +27,14 @@ end
 function Particles(; pos, mom, charge, mass)
     T = eltype(pos)
     npar = size(pos, 2)
+    
+    # Convert 2D arrays to 1D arrays of SVectors
+    positions = [SVector{3,T}(pos[:, i]) for i in 1:npar]
+    momenta = [SVector{3,T}(mom[:, i]) for i in 1:npar]
     efields = zeros(SVector{3,T}, npar)
     bfields = zeros(SVector{3,T}, npar)
-    return Particles(pos, mom, efields, bfields, charge, mass, npar)
+    
+    return Particles(positions, momenta, efields, bfields, charge, mass, npar)
 end
 
 # Algorithm structures
